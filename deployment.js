0a6f963f8d68cd5102a28390d45ab7eeac853815
fairviewbands <97973762+fairviewbands@users.noreply.github.com>
2022-01-30 17:31:36 -0700

    Update deployment.js

--- deployment.js
+++ deployment.js
@@ -1,3 +1,2 @@
 const DEPLOYMENT_ID = `
-AKfycbxIX2g9U-iavdRA7h7CGKDipAWxqhKvB2rj0LAJbcDrks8k1gIAvuti_LGmzb0L459D3g
-`.trim();
+AKfycbxIX2g9U-iavdRA7h7CGKDipAWxqhKvB2rj0LAJbcDrks8k1gIAvuti_LGmzb0L459D3g`.trim();
